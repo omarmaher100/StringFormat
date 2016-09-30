13a4a6113bf91c6e3c428c36bd966379332c1c80
Omar Maher <omarmaher100@gmail.com>
2016-09-30 16:22:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringInCSharp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Omar";
+            string email = "OmarMaher100@gmail.com";
+
+            string oldWay = string.Format("Old Way \n First name : {0} \n Email : {1}", firstName ,email);
+            Console.WriteLine(oldWay);
+            // New in C# 6
+            string newWay = $"New Way in C# 6 \n First name : {firstName} \n Email : {email}";
+            Console.WriteLine(newWay);
+
+        }
+    }
+}
